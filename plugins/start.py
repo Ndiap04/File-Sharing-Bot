d34268ebe4e2c3c07b16c31ac7fdbe08326ae7d6
Ndiap04 <86433202+Ndiap04@users.noreply.github.com>
2021-12-01 13:07:13 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -19,7 +19,7 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
 
 #=====================================================================================##
 
-@Bot.on_message(filters.command('start') & filters.private & subscribed)
+@Bot.on_message(filters.command('help') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
     user_name = '@' + message.from_user.username if message.from_user.username else None
